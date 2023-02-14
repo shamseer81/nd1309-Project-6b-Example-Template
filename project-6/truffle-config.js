1f8c5563f8810d15950c40825ad8e1a6efd13c4d
shamseer81 <shamseer81@gmail.com>
2023-02-14 13:54:26 -0500

    Triage goerli error

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -9,6 +9,15 @@ module.exports = {
       websockets: true
     }
   },
+  goerli: {
+    provider: () => {
+      return new HDWalletProvider('***********************************************************', 
+      'https://goerli.infura.io/v3/' + '***********************************')
+    },
+    network_id: '5', // eslint-disable-line camelcase
+    gas: 4465030,
+    gasPrice: 10000000000,
+  },
   compilers: {
     solc: {
        version: " > 0.5.17",    // Fetch exact version from solc-bin (default: truffle's version)
